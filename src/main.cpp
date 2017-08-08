cc8bf94a80ec8e18b78d6b07197eed6fd7db6c67
bmbigbang <bm.bigbang@gmail.com>
2017-08-08 12:57:36 +0100

    straight line module seemed to work previously but stop after updating the simulator. path planner produces expected values correctly but the car does not respond. final change before taking a new approach to path approximation

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,14 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
+double car_a = 0;
+double prev_car_speed = 0;
+
+// time of prediction into the future which leads to 40 path points with t_base seconds between each
+int path_vals = 50;
+double t_base = 0.02;
+double t_end = path_vals * t_base;
+
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
@@ -281,11 +289,7 @@ int main() {
           auto car_lane = (int) floor(car_d / 3);
           /// target velocity should be 25 m/s for s and small value for d
           double target_s_v = 25;
-          double target_d_v = 0;
 
-          // time of prediction into the future which leads to 40 path points with 0.04s between each
-          int path_vals = 50;
-          double t_end = path_vals * 0.04;
 
           // Previous path data given to the Planner
           auto previous_path_x = j[1]["previous_path_x"];
@@ -309,29 +313,23 @@ int main() {
 
           int target_waypoint = NextWaypoint(car_x, car_y, deg2rad(car_yaw), map_waypoints_x, map_waypoints_y);
 
+          // if waypoint is too close to current pos, choose the next one
+//          if (fabs(target_x*target_x + target_y*target_y - car_x*car_x - car_y*car_y) < 250) {
+//            target_waypoint = target_waypoint + 1;
+//          }
+
           auto target_x = (double) map_waypoints_x[target_waypoint];
           auto target_y = (double) map_waypoints_y[target_waypoint];
           double target_s; double target_dx; double target_dy;
-
-          // if waypoint is too close to current pos, choose the next one
-          if (fabs(target_x*target_x + target_y*target_y - car_x*car_x - car_y*car_y) < 250) {
-            target_x = (double) map_waypoints_x[target_waypoint + 1];
-            target_y = (double) map_waypoints_y[target_waypoint + 1];
-            target_dx = (double) map_waypoints_dx[target_waypoint + 1];
-            target_dy = (double) map_waypoints_dy[target_waypoint + 1];
-            target_s = (double) map_waypoints_s[target_waypoint + 1];
-          }
-          else {
-            target_dx = (double) map_waypoints_dx[target_waypoint];
-            target_dy = (double) map_waypoints_dy[target_waypoint];
-            target_s = (double) map_waypoints_s[target_waypoint];
-          }
+          target_dx = (double) map_waypoints_dx[target_waypoint];
+          target_dy = (double) map_waypoints_dy[target_waypoint];
+          // target_s = (double) map_waypoints_s[target_waypoint];
           // based on the target waypoints calculate heading based on curvature of the road
           double heading = atan2((map_waypoints_y[target_waypoint]-map_waypoints_y[target_waypoint - 1]),
                                  (map_waypoints_x[target_waypoint]-map_waypoints_x[target_waypoint - 1]));
           // calculate heading of the car from the previous path
           int path_size = previous_path_x.size(); double angle; double pos_x; double pos_y;
-          if(path_size == 0)
+          if(path_size <= 1)
           {
             pos_x = car_x;
             pos_y = car_y;
@@ -339,25 +337,37 @@ int main() {
           }
           else
           {
-            pos_x = previous_path_x[path_size-1];
-            pos_y = previous_path_y[path_size-1];
-
-            double pos_x2 = previous_path_x[path_size-2];
-            double pos_y2 = previous_path_y[path_size-2];
+            pos_x = (double) previous_path_x[path_size-1];
+            pos_y = (double) previous_path_y[path_size-1];
+            int index = 1;
+            for (int i = 1; i < path_size; i++) {
+              if (fabs(car_x - pos_x) > fabs(car_x - (double) previous_path_x[i])) {
+                if (fabs(car_y - pos_y) > fabs(car_y - (double) previous_path_y[i])) {
+                  index = i;
+                  pos_x = (double) previous_path_x[index];
+                  pos_y = (double) previous_path_y[index];
+                }
+              }
+            }
+            double pos_x2 = (double) previous_path_x[index - 1];
+            double pos_y2 = (double) previous_path_y[index - 1];
             angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
           }
 
-          next_x_vals.push_back(pos_x);
-          next_y_vals.push_back(pos_y);
-          double target_v_s = car_speed * cos(angle);
-          double target_v_d = car_speed * sin(angle);
-          double target_d = sqrtf(target_dx*target_dx + target_dy*target_dy);
+
+          double target_d = target_dx*target_x + target_dy*target_y;
           double car_v_s = car_speed * cos(angle);
+          double car_v_d = car_speed * sin(angle);
+          double car_a = (car_speed - prev_car_speed) / 0.02;
+          prev_car_speed = car_speed;
+
 
           // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           // search around the road in the next 5 seconds to find if a lane change is necessary
 
           bool change_lane = false;
+          bool slow_down = false;
+          double slow_down_v = 0;
           double s_to_front_car = 0;
           double obj_d;
           double obj_s;
@@ -366,7 +376,16 @@ int main() {
           double obj_yaw;
           double obj_v;
           double obj_v_s;
-          // first check if there is a car in front in the same lane and whether a collision might occur
+
+
+          // if velocity is too slow, speed up in the same lane
+          vector<double> XY; vector<double> SD;
+          double n_s; double n_t; double x; double y; double d; double dx; double dy; double n_d;
+          // change_lane = false;
+          double target_v_s = 0; double target_v_d = 0;
+          n_s = 0;
+
+
           for (int i = 0; i < sensor_fusion.size(); i++) {
             obj_d = sensor_fusion[i][6];
             obj_s = sensor_fusion[i][5];
@@ -374,91 +393,89 @@ int main() {
             obj_y = sensor_fusion[i][2];
             obj_yaw = sensor_fusion[i][4];
             // check the car is in front and in the car's lane
-            if (fabs(obj_d - car_d) < 1.3 && obj_s > car_s) {
+            if (fabs(obj_d - car_d) < 2.1 && obj_s < (car_v_s) * t_end) {
               // check if maintaining current velocity will cause a collision
               obj_yaw = sensor_fusion[i][4];
               obj_v = sensor_fusion[i][3];
               // this is an approximation, but accurate since the other cars dont seem to change lane
-              obj_v_s = obj_v * cos(deg2rad(obj_yaw));
-              if ((obj_v_s * t_end) + obj_s <= (car_v_s * t_end) + car_s) {
-                // flag for lane change
-                change_lane = true;
-                s_to_front_car = obj_s - pos_x;
-              }
-              else {
-                // set straight line waypoints
-                target_d = (car_lane * 4) + 2;
-              }
+              if (s_to_front_car > obj_s) { s_to_front_car = obj_s; }
+              slow_down = true;
+              slow_down_v = obj_v * cos(deg2rad(obj_yaw));
             }
           }
 
-          if (change_lane == true) {
-            for (int i = 0; i < sensor_fusion.size(); i++) {
-              obj_d = (double) sensor_fusion[i][5];
-              obj_s = (double) sensor_fusion[i][6];
-            }
+          double a = 0;
+          if (car_speed < 0) {
+            a = -0.35;
+            target_v_s = car_v_s + (a * t_end);
+          } else if (car_speed < 5) {
+            a = 18;
+            target_v_s = car_v_s + (a * t_end);
+          } else if (car_speed < 10) {
+            a = 0.9;
+            target_v_s = car_v_s + (a * t_end);
+          } else if (car_speed < 18) {
+            a = 0.35;
+            target_v_s = car_v_s + (a * t_end);
+          } else {
+            target_v_s = 18;
+            car_v_s = 18;
+            car_v_d = 0;
+          }
+          if (slow_down) {
+            target_s = s_to_front_car - 1;
+            target_v_s = slow_down_v - 2;
+          } else {
+            target_s = car_v_s * t_end + (0.5 * a * t_end * t_end);
           }
 
-          // if velocity is too slow, speed up in the same lane
-          double n_s; double n_t; vector<double> XY; double x; double y; double d; double ss;
-          // change_lane = false;
+          if (target_s < 0) {
+            target_s = target_v_s * t_end;
+          }
 
+          // target_d = ((car_lane - 1) * 4) + 2.16;
+          // d = target_d + 6;
+          // max sure max acceleration of 2m/s**2 for s, and 0.05m/s**2 for d is complied with
+          // SD = getFrenet(car_x, car_y, angle, map_waypoints_x, map_waypoints_y);
 
-          if (car_speed < 55 and !change_lane) {
+          // solve jerk minimizing trajectory for proposed goal and start states
+          vector<double> jmt_s = JMT({0, car_v_s, car_a * cos(heading)}, {target_s, target_v_s, 0}, t_end);
+          vector<double> jmt_d = JMT({0, car_v_d, car_a * sin(heading)}, {6.16 - car_d, 0, 0}, t_end);
 
-            n_s = 0;
-            d = ((target_d - car_d) / path_vals);
-            double a_2 = heading;
-            for (int i = 0; i < path_vals; i++) {
-              // max acceleration should be 4m/s**2 such that over one second velocity may increment by roughly 4
-              target_s = (car_v_s + 2) * t_end;
-//              if (change_lane == true) {
-//                // avoid speeding up past collision path
-//                if (target_s > s_to_front_car - 5) {
-//                  target_s = s_to_front_car - 5;
-//                }
-//              }
+          cout << 6.16 - car_d << " - " << target_s << " - " << car_x << " - " << car_y << " - " << slow_down << endl;
+          cout << " ---------------------- " << endl;
+          int count = 0;
+          // interpolate path points
+          for (int i = 0; i < path_vals; i++) {
+            n_t = t_base * i;
+            n_s = jmt_s[0] + jmt_s[1] * n_t + jmt_s[2] * (n_t * n_t) + jmt_s[3] * (n_t * n_t * n_t) +
+                  jmt_s[4] * (n_t * n_t * n_t * n_t) + jmt_s[5] * (n_t * n_t * n_t * n_t * n_t);
+            n_d = jmt_d[0] + jmt_d[1] * n_t + jmt_d[2] * (n_t * n_t) + jmt_d[3] * (n_t * n_t * n_t) +
+                  jmt_d[4] * (n_t * n_t * n_t * n_t) + jmt_d[5] * (n_t * n_t * n_t * n_t * n_t);
 
-              n_t = (i + 1.0) / path_vals;
-              x = pos_x + ((n_t * target_s) * cos(a_2));
-              y = pos_y + ((n_t * target_s) * sin(a_2));
+//              dx = target_dx + (n_d * cos(a_2 - pi()/2));
+//              dy = target_dy + (n_d * sin(a_2 - pi()/2));
+            // the x,y,s along the segment
+            // XY = getXY(car_s + n_s, car_d + n_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+            x = car_x + (n_s * cos(heading)) + (n_d * cos(heading - pi()/2));
+            y = car_y + (n_s * sin(heading)) + (n_d * sin(heading - pi()/2));
+
+            cout << x << " - " << y << " - " << (n_s * cos(heading)) << " - " << n_d * cos(heading - pi()/2) << endl;
+            if (count and x > next_x_vals[count]) {
+              count += 1;
               next_x_vals.push_back(x);
               next_y_vals.push_back(y);
-
             }
           }
-          else {
 
-            // solve jerk minimizing trajectory for proposed goal and start states
-            vector<double> jmt_s = JMT({car_s, car_v_s, 0}, {car_s + 40, target_s_v, 1}, t_end);
-            vector<double> jmt_d = JMT({car_d, 0, 0}, {car_d, target_d_v, 0.1}, t_end);
-
-            // interpolate path points
-
-//            for (int i = 0; i < path_vals; i++) {
-//              n_t = 0.04 * (i + 1);
-//              n_s = jmt_s[0] + jmt_s[1] * n_t + jmt_s[2] * (n_t * n_t) + jmt_s[3] * (n_t * n_t * n_t) +
-//                    jmt_s[4] * (n_t * n_t * n_t * n_t) + jmt_s[5] * (n_t * n_t * n_t * n_t * n_t);
-//              n_d = jmt_d[0] + jmt_d[1] * n_t + jmt_d[2] * (n_t * n_t) + jmt_d[3] * (n_t * n_t * n_t) +
-//                    jmt_d[4] * (n_t * n_t * n_t * n_t) + jmt_d[5] * (n_t * n_t * n_t * n_t * n_t);
-
-              // the x,y,s along the segment
-//              double seg_s = (s-maps_s[prev_wp]);
-//
-//              double seg_x = maps_x[prev_wp]+seg_s*cos(heading);
-//              double seg_y = maps_y[prev_wp]+seg_s*sin(heading);
-//              XY = getXY(n_s, n_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-//              next_x_vals[i] = XY[0];
-//              next_y_vals[i] = XY[1];
-//            }
-          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
           auto msg = "42[\"control\","+ msgJson.dump()+"]";
 
-          //this_thread::sleep_for(chrono::milliseconds(1000));
+          // this_thread::sleep_for(chrono::milliseconds(160));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
 
         }
